e47437d047ae2720663d79401f27eaef27bfdfea
kunal <kunal.dangare@gmail.com>
2023-02-21 14:44:46 +0530

    second commit

--- src/test/java/Selenium_all_Programs/Selenium_all_Programs/gitDemo.java
+++ src/test/java/Selenium_all_Programs/Selenium_all_Programs/gitDemo.java
@@ -3,7 +3,7 @@ package Selenium_all_Programs.Selenium_all_Programs;
 public class gitDemo {
 
 	public static void main(String[] args) {
-	System.out.println("practice git");
+	System.out.println("practice git changes");
 
 	}
 
